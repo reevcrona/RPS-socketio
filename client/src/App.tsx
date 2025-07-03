6921b279869762820d467294aa864462b8ce3c92
Jreev <jacob.reevcrona@yahoo.com>
2025-07-03 13:04:39 +0200

    add: client and server are now connected and can emit events

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
-
+import { useState, useEffect } from "react";
+import { socket } from "./socket";
 function App() {
+  const [isConnected, setIsConected] = useState(socket.connected);
+
+  useEffect(() => {
+    const onConnect = () => {
+      setIsConected(true);
+    };
+
+    socket.on("connect", onConnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+    };
+  }, []);
+
   return (
     <>
-      <h1>test</h1>
+      <h1>Am i connected? - {isConnected ? "Yes" : "No"}</h1>
     </>
   );
 }
--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL =
-  import.meta.env.VITE.SERVER.URL === "production"
-    ? undefined
-    : "http://localhost:3000";
+const URL = "http://localhost:4000";
 
 export const socket = io(URL);
