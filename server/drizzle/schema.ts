1c256735cbdbc4b656ed3b83a507a1b0b751d9d3
Jreev <jacob.reevcrona@yahoo.com>
2025-07-07 13:12:24 +0200

    add: messaged table for the database

--- server/drizzle/schema.ts
+++ server/drizzle/schema.ts
@@ -1,8 +1,14 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const messages = pgTable("messages", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  content: text("content").notNull(),
+  createAt: timestamp("created_at").defaultNow().notNull(),
 });
