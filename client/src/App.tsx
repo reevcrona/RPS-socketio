24446d401fd94285172258b0ac8d4f4e784dc3d1
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-07-09 16:51:41 +0200

    add: Home and Lobby pages to start separating things

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,91 +1,20 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Lobby from "./pages/Lobby";
+import { useState } from "react";
 import { socket } from "./socket/socket";
 import { useSocketListeners } from "./hooks/useSocketListeners";
-import { FaCircle } from "react-icons/fa";
-import MessageList from "./components/MessageList";
-import LobbyContainer from "./components/Lobby/LobbyContainer";
-import LightBox from "./components/Lightbox";
-import LobbyOptions from "./components/Lobby/LobbyOptions";
-import LobbyList from "./components/Lobby/LobbyList";
-import { useSocketEmitters } from "./hooks/useSocketEmitters";
-import UserNameComponent from "./components/UserNameComponent";
 function App() {
-  const { sendMessageToServer } = useSocketEmitters();
-
-  const [userName, setUserName] = useState<string>("");
-
-  const [inputValue, setInputValue] = useState<string>("");
   const [isConnected, setIsConected] = useState(socket.connected);
-  const [showLightbox, setShowLightbox] = useState<boolean>(false);
-  const [showUsernameLightbox, setShowUsernameLightbox] =
-    useState<boolean>(true);
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    setterFunc: React.Dispatch<React.SetStateAction<string>>
-  ) => {
-    setterFunc(e.target.value);
-  };
   useSocketListeners(setIsConected);
   return (
-    <div className="flex flex-col  items-center w-full">
-      <div className="flex items-center mb-7">
-        <FaCircle
-          className={`mr-6 text-5xl ${
-            isConnected ? "text-green-500" : "text-red-500"
-          }`}
-        />
-        <h1
-          className={`${
-            isConnected ? "text-green-500" : "text-red-500"
-          } text-4xl`}
-        >
-          {isConnected ? "Connected" : "No connection"}
-        </h1>
-      </div>
-
-      <input
-        className="bg-black text-white"
-        placeholder="text here"
-        type="text"
-        value={inputValue}
-        onChange={(e) => handleChange(e, setInputValue)}
-      />
-      <button
-        onClick={() => sendMessageToServer(inputValue)}
-        className="text-white bg-black mt-3 p-2 rounded-xl"
-      >
-        Send message
-      </button>
-
-      <div>
-        <MessageList />
-      </div>
-
-      {userName && <h2>User: {userName}</h2>}
-
-      <LobbyContainer setShowLightbox={setShowLightbox}>
-        <LobbyList />
-      </LobbyContainer>
-
-      {showUsernameLightbox && (
-        <LightBox>
-          <UserNameComponent
-            handleChange={handleChange}
-            setShowUsernameLightbox={setShowUsernameLightbox}
-            setUserName={setUserName}
-            userName={userName}
-          />
-        </LightBox>
-      )}
-
-      {showLightbox && (
-        <LightBox>
-          <LobbyOptions setShowLightbox={setShowLightbox} />
-        </LightBox>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home isConnected={isConnected} />} />
+        <Route path="/lobby/:id" element={<Lobby />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
